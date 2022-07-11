c395579fdaf89955ad6d17d155bc34105d43ef8a
user.name <2998909624@qq.com>
2022-07-11 16:20:30 +0800

    first git

---
+++ src/main/java/com/Snd.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Snd {
+    public void xn(){
+        System.out.println("xxxxxx");
+        System.out.println("xxxxxx");
+        System.out.println("xxxxxx");
+        System.out.println("xxxxxx");
+    }
+}
